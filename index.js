28cf0b078ca6e7c2ac562fa9884a19ae4bcfdc60
EAGLE1309 <vighneshbangar57@gmail.com>
2023-10-24 18:00:23 +0530

    Finalised the project

--- index.js
+++ index.js
@@ -1,87 +1,70 @@
 #!/usr/bin/env node
 
-// Import packages
 import chalk from 'chalk';
 import boxen from 'boxen';
 import { input, confirm } from '@inquirer/prompts';
+import { Command } from 'commander';
 
-// Import local modules
-import boot from "./utility/boot.js"
-import title from "./utility/title.js"
-import wait from "./utility/wait.js"
+import boot from "./utility/boot.js";
+import title from "./utility/title.js";
+import wait from "./utility/wait.js";
 import { search } from "./helpers/wiki.js";
-import { objToPairs, output } from "./utility/objEntries.js"
+import { objToPairs, output } from "./utility/objEntries.js";
 import { readCache, writeCache, clearCache } from "./helpers/cache.js";
 
-import { Command } from 'commander';
 const program = new Command();
 
-program
-  .name('wiki-cli')
-  .description('Awesome and interactive wikipedia cli for nodejs')
-  .version("1.0.0");
+const displayError = (query, err) => {
+  console.error(chalk.red(boxen(`Couldn't find search results for ${query}\n${err}`, { padding: 1, borderColor: "red" })));
+};
 
-// Error Handling
-const displayError = (query = "", err) => {
-  // Return error in a red box for better ui
-  console.log(chalk.red(boxen(`Couldn't find search results for ${query}\n${err}`, { padding: 1, borderColor: "red" })))
-}
-
-// Search Result
 const displaySearchResult = async (result) => {
-  const content = boxen(result.content, { padding: 1, borderColor: "gray", title: "Basic info" })
-  const detailsObj = await result.details
+  const content = boxen(result.content, { padding: 1, borderColor: "gray", title: "Basic info" });
+  const detailsObj = await result.details;
 
-  await console.log(content, "\n");
+  console.log(content, "\n");
   await objToPairs(detailsObj);
-  console.log(boxen(output, { padding: 1, borderColor: "gray", title: "Details" }))
-}
+  console.log(boxen(output, { padding: 1, borderColor: "gray", title: "Details" }));
+};
 
-// Main Function
+const searchAndCache = async (query, cache) => {
+  try {
+    const result = await search(query);
+    displaySearchResult(result);
+    if (result) {
+      cache[query] = result;
+      writeCache(cache);
+    }
+  } catch (err) {
+    displayError(query, err);
+  }
+};
+
 const main = async () => {
+  console.clear();
+  await boot();
+  await title();
+  await wait(1000);
 
-  console.clear(); // Clear console
-
-  await boot(); // Start function
-  await title(); // Title for whole script
-  await wait(1000); // Load for 1 sec
-
-  // Get Input (interactive)
-  const query = await input({ message: "Search: ", default: "Bleach anime" });
+  const query = (await input({ message: "Search: ", default: "Bleach anime" })).trim();
 
   if (!query) {
     console.error(chalk.red("Error: Query cannot be empty."));
     return;
   }
 
-  // Load cache
   const cache = readCache();
 
-  // Check if the query is in the cache
   if (cache[query]) {
     displaySearchResult(cache[query]);
     return;
   }
 
-  // Search for the given term (query) using wiki.js
-
-  await search(query)
-    .then(result => {
-      displaySearchResult(result);
-      if (result) {
-        cache[query] = result;
-        writeCache(cache);
-      }
-    })
-    .catch(err => displayError(query, err))
-  // for. e.g. search("Bleach Anime")
-
-}
+  await searchAndCache(query, cache);
+};
 
 const quickMain = async (query) => {
-
   console.clear();
-
   await title();
   await wait(250);
 
@@ -89,43 +72,45 @@ const quickMain = async (query) => {
     console.error(chalk.red("Error: Query cannot be empty."));
     return;
   }
-
-  // Load cache
   const cache = readCache();
-
-  // Check if the query is in the cache
   if (cache[query]) {
     displaySearchResult(cache[query]);
     return;
   }
+  await searchAndCache(query, cache);
+};
 
-  // Search for the given term (query) using wiki.js
 
-  await search(query)
-    .then(result => {
-      displaySearchResult(result);
-      if (result) {
-        cache[query] = result;
-        writeCache(cache);
-      }
-    })
-    .catch(err => displayError(query, err))
-  // for. e.g. search("Bleach Anime")
+// Main Program
+program
+  .name('wiki-cli')
+  .description('Awesome and interactive Wikipedia CLI for Node.js')
+  .version("1.0.0")
 
-}
-
-program.command('search')
-  .description('Search for your query wikipedia')
-  .argument('<string>', 'query to search')
+program.command('search <query>')
+  .description('Search for your query on Wikipedia')
   .action((query) => {
-    quickMain(query)
+    quickMain(query);
   });
 
-program.command("interact")
-  .description("Opens CLI in interactive mode")
+program.command('interact')
+  .description('Opens CLI in interactive mode')
   .action(() => {
     main();
-  })
+  });
+
+program.command('cache-clean')
+  .description('Clears cache')
+  .action(async () => {
+    const answer = await confirm({ message: 'Continue?' });
+    if (answer) {
+      clearCache()
+      console.log(chalk.magenta("Cleared Cache Successfully ✅"));
+    }
+    else {
+      console.log(chalk.magenta("Cancelled ❌"))
+      return;
+    }
+  });
 
 program.parse();
-// main();
