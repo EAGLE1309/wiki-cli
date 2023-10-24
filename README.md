b2f7e4f58846c80ebe7c7b24e3b599d9a91f084a
EAGLE1309 <vighneshbangar57@gmail.com>
2023-10-24 22:44:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wiki Cli
+
+To use it do:
+
+```
+git clone https://github.com/EAGLE1309/wikipedia-cli
+```
+```
+cd wikipedia-cli
+```
+```
+npm i
+```
+```
+node .
+```
--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 import chalk from 'chalk';
 import boxen from 'boxen';
-import { input, confirm } from '@inquirer/prompts';
-import { Command } from 'commander';
+import { input, confirm, select } from '@inquirer/prompts';
 
 import boot from "./utility/boot.js";
 import title from "./utility/title.js";
@@ -11,8 +10,6 @@ import wait from "./utility/wait.js";
 import { search } from "./helpers/wiki.js";
 import { objToPairs, output } from "./utility/objEntries.js";
 import { readCache, writeCache, clearCache } from "./helpers/cache.js";
-
-const program = new Command();
 
 const displayError = (query, err) => {
   console.error(chalk.red(boxen(`Couldn't find search results for ${query}\n${err}`, { padding: 1, borderColor: "red" })));
@@ -30,7 +27,7 @@ const displaySearchResult = async (result) => {
 const searchAndCache = async (query, cache) => {
   try {
     const result = await search(query);
-    displaySearchResult(result);
+    await displaySearchResult(result);
     if (result) {
       cache[query] = result;
       writeCache(cache);
@@ -46,71 +43,56 @@ const main = async () => {
   await title();
   await wait(1000);
 
-  const query = (await input({ message: "Search: ", default: "Bleach anime" })).trim();
+  const operate = await select({
+    message: "What you wanna do?",
+    choices: [
+      {
+        name: "1. Search",
+        value: "search",
+        description: chalk.gray("Search for your query")
+      },
+      {
+        name: "2. Clear Cache",
+        value: "clear_cache",
+        description: chalk.gray("Clear Cache of (your search history)")
+      },
+      {
+        name: "3. Exit",
+        value: "exit",
+        description: chalk.gray("Exit the cli app")
+      }
+    ]
+  })
 
-  if (!query) {
-    console.error(chalk.red("Error: Query cannot be empty."));
-    return;
+  switch (operate) {
+    case "search":
+
+      const query = (await input({ message: "Search: ", default: "Bleach anime" })).trim();
+
+      if (!query) {
+        console.error(chalk.red("Error: Query cannot be empty."));
+        return;
+      }
+
+      const cache = readCache();
+
+      if (cache[query]) {
+        displaySearchResult(cache[query]);
+        return;
+      }
+
+      await searchAndCache(query, cache);
+      break;
+
+    case "clear_cache":
+      clearCache();
+      console.log(chalk.black.bgGreen("Success"), chalk.magenta("Successfully cleared the cache."))
+      break;
+
+    case "exit":
+      console.log(chalk.magenta(chalk.black.bgGreen("Success"), "Exited process with status code (1)"))
+      process.exit(1)
   }
-
-  const cache = readCache();
-
-  if (cache[query]) {
-    displaySearchResult(cache[query]);
-    return;
-  }
-
-  await searchAndCache(query, cache);
 };
 
-const quickMain = async (query) => {
-  console.clear();
-  await title();
-  await wait(250);
-
-  if (!query) {
-    console.error(chalk.red("Error: Query cannot be empty."));
-    return;
-  }
-  const cache = readCache();
-  if (cache[query]) {
-    displaySearchResult(cache[query]);
-    return;
-  }
-  await searchAndCache(query, cache);
-};
-
-
-// Main Program
-program
-  .name('wiki-cli')
-  .description('Awesome and interactive Wikipedia CLI for Node.js')
-  .version("1.0.0")
-
-program.command('search <query>')
-  .description('Search for your query on Wikipedia')
-  .action((query) => {
-    quickMain(query);
-  });
-
-program.command('interact')
-  .description('Opens CLI in interactive mode')
-  .action(() => {
-    main();
-  });
-
-program.command('cache-clean')
-  .description('Clears cache')
-  .action(async () => {
-    const answer = await confirm({ message: 'Continue?' });
-    if (answer) {
-      clearCache()
-      console.log(chalk.magenta("Cleared Cache Successfully ✅"));
-    }
-    else {
-      console.log(chalk.magenta("Cancelled ❌"))
-      return;
-    }
-  });
-
-program.parse();
+main();
